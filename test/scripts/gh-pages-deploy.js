3b9f4dda1e0b8fa4ae6abb9a9ded970c27f8a2fc
doubleapple123 <leonychen123@gmail.com>
2022-04-15 21:54:49 -0700

    .

--- test/scripts/gh-pages-deploy.js
+++ test/scripts/gh-pages-deploy.js
@@ -4,8 +4,8 @@ const fs = require("fs");
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
-    console.log("Building started...");
-    await execa("npm", ["run", "build"]);
+/*    console.log("Building started...");
+    await execa("npm", ["run", "build"]);*/
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
